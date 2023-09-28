eb053520153916900a0433cb0ad1c3e562aae849
Tarbiee <dorcusstaby@gmail.com>
2023-09-28 23:52:50 +0300

    Completed changes

--- index2.js
+++ index2.js
@@ -94,58 +94,6 @@ function netSalary(){
     }
     console.log(`NHIFDeduction: ${nhifDeductions}`);//displays calculated nhifdeductions 
 
-    // if(grossSalary <= 5999){
-    //     nhifDeductions = 150;;
-    // }
-    // else if (grossSalary >= 6000 && grossSalary <= 7999){
-    //     nhifDeductions = 300;
-    // }
-    // else if (grossSalary >= 8000 && grossSalary <= 11999){
-    //     nhifDeductions = 400;
-    // }
-    // else if (grossSalary >= 12000 && grossSalary <= 14999){
-    //     nhifDeductions = 500;
-    // }
-    // else if (grossSalary >= 15000 && grossSalary <= 19999){
-    //     nhifDeductions = 600;
-    // }
-    // else if (grossSalary >= 20000 && grossSalary <= 24999){
-    //     nhifDeductions = 750;
-    // }
-    // else if (grossSalary >= 25000 && grossSalary <= 29999){
-    //     nhifDeductions = 850;
-    // }
-    // else if (grossSalary >= 30000 && grossSalary <= 34999){
-    //     nhifDeductions = 900;
-    // }
-    // else if (grossSalary >= 35000 && grossSalary <= 39999){
-    //     nhifDeductions = 950;
-    // }
-    // else if (grossSalary >= 40000 && grossSalary <= 44999){
-    //     nhifDeductions = 1000;
-    // }
-    // else if (grossSalary >= 45000 && grossSalary <= 49999){
-    //     nhifDeductions = 1100;
-    // }
-    // else if (grossSalary >= 50000 && grossSalary <= 59999){
-    //     nhifDeductions = 1200;
-    // }
-    // else if (grossSalary >= 60000 && grossSalary <= 69999){
-    //     nhifDeductions = 1300;
-    // }
-    // else if (grossSalary >= 70000 && grossSalary <= 79999){
-    //     nhifDeductions = 1400;
-    // }
-    // else if (grossSalary >= 80000 && grossSalary <= 89999){
-    //     nhifDeductions = 1500;
-    // }
-    // else if (grossSalary >= 90000 && grossSalary <= 99999){
-    //     nhifDeductions = 1600;
-    // }
-    // else if (grossSalary >= 100000){
-    //     nhifDeductions = 1700;
-    // }
-    
 
     // NSSF Deductions
 //Calculates nssfDeductions based on gross salary
